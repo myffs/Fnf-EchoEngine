04d19d0aa81c0b570886098b636da188a61ab19b
MyFnf <113801267+LilDrippyMyFnf@users.noreply.github.com>
2022-12-19 17:01:13 -0500

    unused script (delete '--' so you can use)

---
+++ Source/Scripts/RatingState.lua
@@ -0,0 +1,38 @@
+--function onUpdate()
+
+    --if ratingName == 'Perfect!!' then
+
+        --setRatingName('Perfecto!!')
+
+    --end
+
+    --if ratingName == 'Sick!' then
+
+      --  setRatingName('Amazing!')
+
+    --end
+
+    --if ratingName == 'Great' then
+
+      --  setRatingName('Sick!')
+
+    --end
+
+    --if ratingName == 'Good' then
+
+      --  setRatingName('Great!')
+
+    --end
+
+    --if ratingName == 'Meh' then
+
+      --  setRatingName('dull...')
+
+    --end
+
+    --if ratingName == 'bad' then
+
+      --  setRatingName('fuck you!!')
+
+   -- end
+  --end
