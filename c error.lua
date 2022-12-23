e6e0c2b0b142595061c33adec75ddf5ae5840e97
SlacDeveloper <121267818+SlacDeveloper@users.noreply.github.com>
2022-12-23 19:03:38 -0400

    create c error.lua
    What this lua does is that at the beginning of the song an error called ''c'' will appear
    
    note: THIS LUA IS BROKEN

---
+++ c error.lua
@@ -0,0 +1,10 @@
+-- this is not a real lua, i don't recommend using it, unless you want there to be an error called ''c''
+function onCreatePost()
+    create text()
+        'callate estupido :clown:
+                function noteMissPress(direction)
+            -- Called after the note press miss calculations
+            -- Player pressed a button, but there was no note to                 hit (ghost miss)
+        end
+    end
+end
