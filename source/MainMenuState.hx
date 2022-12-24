328e05b7c34575ecc2e8a57955e9d7ee9c71047c
min89xd <90361729+min89xd@users.noreply.github.com>
2022-12-23 23:53:20 -0600

    Revert "updated"
    This reverts commit 5da3deda22e9f078dec218b4fee0b8a65512c5f7.

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -26,7 +26,7 @@ using StringTools;
 class MainMenuState extends MusicBeatState
 {
 	public static var psychEngineVersion:String = '0.6.3'; //This is also used for Discord RPC
-	public static var echoEngineVersion:String = '0.2.2'; //yeah baby
+	public static var echoEngineVersion:String = '0.2.0'; //yeah baby
 	public static var curSelected:Int = 0;
 
 	var menuItems:FlxTypedGroup<FlxSprite>;
