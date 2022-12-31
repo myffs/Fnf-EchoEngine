15007a786fca6f1a0ba470f7b3e5c51353b63b20
jmxamongusmodder <80003646+jmxamongusmodder@users.noreply.github.com>
2022-12-31 01:16:04 -0500

    Use this one with caution

---
+++ art/html5 release.bat
@@ -0,0 +1,6 @@
+@echo off
+echo starting
+lime build html5
+echo its done
+echo now you can use this engine but on websites btw this is super unstable and not even tested yet use with caution
+pause
