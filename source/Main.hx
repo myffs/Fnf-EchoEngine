af31970639b518c0729db2a35e08f036e40d33a7
Stefan[BETA]™ <stefanbeta2008@gmail.com>
2023-01-01 01:30:34 +0100

    Alright, you are really forget it for seriously

--- source/Main.hx
+++ source/Main.hx
@@ -132,7 +132,7 @@ class Main extends Sprite
 		dateNow = dateNow.replace(" ", "_");
 		dateNow = dateNow.replace(":", "'");
 
-		path = "./crash/" + "PsychEngine_" + dateNow + ".txt";
+		path = "./crash/" + "EchoEngine_" + dateNow + ".txt";
 
 		for (stackItem in callStack)
 		{
@@ -145,7 +145,7 @@ class Main extends Sprite
 			}
 		}
 
-		errMsg += "\nUncaught Error: " + e.error + "\nPlease report this error to the GitHub page: https://github.com/ShadowMario/FNF-PsychEngine\n\n> Crash Handler written by: sqirra-rng";
+		errMsg += "\nUncaught Error: " + e.error + "\nPlease report this error to the GitHub page: https://github.com/LilDrippyMyFnf/Fnf-EchoEngine\n\n> Crash Handler written by: sqirra-rng";
 
 		if (!FileSystem.exists("./crash/"))
 			FileSystem.createDirectory("./crash/");
