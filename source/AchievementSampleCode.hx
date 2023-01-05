388dc3cc948755614421ca40b28d82868b83f4ab
OakShubs <121911898+OakShubs@users.noreply.github.com>
2023-01-05 00:21:44 -0500

    Achievement Template
    For those who don't understand the achievements.hx file

---
+++ source/AchievementSampleCode.hx
@@ -0,0 +1,11 @@
+Package;
+
+import.flixel.flxsprite
+import.flixel.flxG
+// their was more but I forgot
+
+using stringtools;
+
+Class template = ["Achievement Name","Description","Icon"];
+// some tutorial code on achievements.hx
+trace(template);
