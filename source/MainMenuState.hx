d2e0d4f8244284d2a4066ea0397e44ba85738c33
MyFnf <113801267+LilDrippyMyFnf@users.noreply.github.com>
2023-02-17 15:27:22 -0500

    yes, echo engine will be restored soon :happy:

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -28,7 +28,7 @@ using StringTools;
 class MainMenuState extends MusicBeatState
 {
 	public static final psychEngineVersion:String = '0.6.3'; // This is also used for Discord RPC
-	public static final echoEngineVersion:String = '0.4.2'; // yeah baby
+	public static final echoEngineVersion:String = '0.4.8'; // A FUCKING RESTORED UPDATE
 	public static var curSelected:Int = 0;
 
 	var menuItems:FlxTypedGroup<FlxSprite>;
