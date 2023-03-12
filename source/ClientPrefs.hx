6e69ada38d14662a71848aa49a898a3398ab389e
MyFnf <113801267+LilDrippyMyFnf@users.noreply.github.com>
2023-03-12 00:26:12 -0500

    pfft, fuck off source code..

--- source/ClientPrefs.hx
+++ source/ClientPrefs.hx
@@ -34,6 +34,7 @@ class ClientPrefs {
 	public static var comboStacking = true;
 	public static var catMode = true;
 	public static var multiPlayer = true;// hey memehoovy am i gonna have to code this in?
+	public static var androidPort = true;
 	public static var gameplaySettings:Map<String, Dynamic> = [
 		'scrollspeed' => 1.0,
 		'scrolltype' => 'multiplicative', 
